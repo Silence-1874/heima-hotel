edaf658186392a26a4c21938e46ab72d5f15bb70
Silence <silence1874@foxmail.com>
2023-03-09 14:51:58 +0800

    test(ES): Get document

--- src/test/java/cn/itcast/hotel/HotelDocumentTest.java
+++ src/test/java/cn/itcast/hotel/HotelDocumentTest.java
@@ -5,6 +5,8 @@ import cn.itcast.hotel.pojo.HotelDoc;
 import cn.itcast.hotel.service.impl.HotelService;
 import com.alibaba.fastjson.JSON;
 import org.apache.http.HttpHost;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
@@ -33,6 +35,15 @@ public class HotelDocumentTest {
         IndexRequest request = new IndexRequest("hotel").id(hotel.getId().toString());
         request.source(JSON.toJSONString(hotelDoc), XContentType.JSON);
         client.index(request, RequestOptions.DEFAULT);
+    }
+
+    @Test
+    void testGetDocument() throws IOException {
+        GetRequest request = new GetRequest("hotel", "61083");
+        GetResponse response = client.get(request, RequestOptions.DEFAULT);
+        String json = response.getSourceAsString();
+        HotelDoc hotelDoc = JSON.parseObject(json, HotelDoc.class);
+        System.out.println(hotelDoc);
     }
 
     @BeforeEach
