2ac598334c650374888b43c931cb6be1cb92368d
Silence <silence1874@foxmail.com>
2023-03-09 10:31:29 +0800

    test(ES): Create index

--- src/test/java/cn/itcast/hotel/HotelIndexTest.java
+++ src/test/java/cn/itcast/hotel/HotelIndexTest.java
@@ -1,11 +1,12 @@
 package cn.itcast.hotel;
 
-import com.baomidou.mybatisplus.extension.api.R;
+import cn.itcast.hotel.constants.HotelConstants;
 import org.apache.http.HttpHost;
-import org.apache.http.client.methods.HttpPost;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
-import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,6 +19,16 @@ public class HotelIndexTest {
     @Test
     void testInit() {
         System.out.println(client);
+    }
+
+    @Test
+    void testCreateHotelIndex() throws IOException {
+        // 1.创建 Request 对象
+        CreateIndexRequest request = new CreateIndexRequest("hotel");
+        // 2.请求参数，MAPPING_TEMPLATE 是DSL语句的静态常量
+        request.source(HotelConstants.MAPPING_TEMPLATE, XContentType.JSON);
+        // 3.发送请求
+        client.indices().create(request, RequestOptions.DEFAULT);
     }
 
     @BeforeEach
